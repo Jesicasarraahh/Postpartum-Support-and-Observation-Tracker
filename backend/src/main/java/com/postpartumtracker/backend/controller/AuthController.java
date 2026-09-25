a3d38b32ca1c5f06ba2109bc7e45888b82747e3b
Jesica Sarah <jesicasarah@Jesicas-MacBoo-Air.local>
2026-09-24 20:52:47 -0400

    Creating login endpoint

--- backend/src/main/java/com/postpartumtracker/backend/controller/AuthController.java
+++ backend/src/main/java/com/postpartumtracker/backend/controller/AuthController.java
@@ -8,15 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.postpartumtracker.backend.dto.UserResponse;
+import com.postpartumtracker.backend.dto.LoginRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final UserService userService;
-
-    public AuthController(UserService userService) {
+    private final AuthenticationManager authenticationManager;
+    public AuthController(UserService userService,
+        AuthenticationManager authenticationManager
+    ) 
+    {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
@@ -27,4 +34,17 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+    @PostMapping("/login")
+public ResponseEntity<String> login(
+        @Valid @RequestBody LoginRequest request) {
+
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail().toLowerCase(),
+            request.getPassword()
+        )
+    );
+
+    return ResponseEntity.ok("Login successful");
+}
 }
